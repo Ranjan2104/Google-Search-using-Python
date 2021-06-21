8c5515fd7326271c35dea2b212a308ec787ff475
Amresh2104 <60054130+Ranjan2104@users.noreply.github.com>
2021-06-21 12:51:43 +0530

    Main files

---
+++ Main.py
@@ -0,0 +1,13 @@
+# Amresh Ranjan.
+
+import pywhatkit as kt
+
+print('Lets perform Google search!')
+
+target1 = 'facebook'
+target2 = 'Java'
+target3 = 'C++'
+target4 = 'python'
+
+kt.search(target2)
+
